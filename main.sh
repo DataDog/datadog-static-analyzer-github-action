aef03e0f19e998a163aed06c36936c0f4ac3fe11
Amaan Qureshi <amaan.qureshi@datadoghq.com>
2024-06-20 11:30:30 -0400

    just use entrypoint

--- main.sh
+++ main.sh
@@ -4,58 +4,58 @@
 # check variables
 ########################################################
 if [ -z "$DD_API_KEY" ]; then
-    echo "DD_API_KEY not set. Please set one and try again."
-    exit 1
+	echo "DD_API_KEY not set. Please set one and try again."
+	exit 1
 fi
 
 if [ -z "$DD_APP_KEY" ]; then
-    echo "DD_APP_KEY not set. Please set one and try again."
-    exit 1
+	echo "DD_APP_KEY not set. Please set one and try again."
+	exit 1
 fi
 
 if [ -z "$DD_ENV" ]; then
-    echo "DD_ENV not set. Please set this variable and try again."
-    exit 1
+	echo "DD_ENV not set. Please set this variable and try again."
+	exit 1
 fi
 
 if [ -z "$DD_SERVICE" ]; then
-    echo "DD_SERVICE not set. Please set this variable and try again."
-    exit 1
+	echo "DD_SERVICE not set. Please set this variable and try again."
+	exit 1
 fi
 
 if [ -z "$CPU_COUNT" ]; then
-    # the default CPU count is 2
-    CPU_COUNT=2
+	# the default CPU count is 2
+	CPU_COUNT=2
 fi
 
 if [ "$ENABLE_PERFORMANCE_STATISTICS" = "true" ]; then
-    ENABLE_PERFORMANCE_STATISTICS="--performance-statistics"
+	ENABLE_PERFORMANCE_STATISTICS="--performance-statistics"
 else
-    ENABLE_PERFORMANCE_STATISTICS=""
+	ENABLE_PERFORMANCE_STATISTICS=""
 fi
 
 if [ "$ENABLE_DEBUG" = "yes" ]; then
-    DEBUG_ARGUMENT_VALUE="yes"
+	DEBUG_ARGUMENT_VALUE="yes"
 else
-    DEBUG_ARGUMENT_VALUE="no"
+	DEBUG_ARGUMENT_VALUE="no"
 fi
 
 if [ -n "$SUBDIRECTORY" ]; then
-  for subdirectory in $SUBDIRECTORY; do
-    SUBDIRECTORY_OPTION="$SUBDIRECTORY_OPTION --subdirectory $subdirectory"
-  done
+	for subdirectory in $SUBDIRECTORY; do
+		SUBDIRECTORY_OPTION="$SUBDIRECTORY_OPTION --subdirectory $subdirectory"
+	done
 fi
 
 if [ "$DIFF_AWARE" = "true" ]; then
-    DIFF_AWARE_VALUE="--diff-aware"
+	DIFF_AWARE_VALUE="--diff-aware"
 else
-    DIFF_AWARE_VALUE=""
+	DIFF_AWARE_VALUE=""
 fi
 
 # verify ARCHITECTURE is x86_64 or aarch64
 if [ "$ARCHITECTURE" != "x86_64" ] && [ "$ARCHITECTURE" != "aarch64" ]; then
-    echo "ARCHITECTURE must be x86_64 or aarch64"
-    exit 1
+	echo "ARCHITECTURE must be x86_64 or aarch64"
+	exit 1
 fi
 
 ########################################################
@@ -64,8 +64,8 @@ fi
 TOOL_DIRECTORY=$(mktemp -d)
 
 if [ ! -d "$TOOL_DIRECTORY" ]; then
-    echo "Tool directory $TOOL_DIRECTORY does not exist"
-    exit 1
+	echo "Tool directory $TOOL_DIRECTORY does not exist"
+	exit 1
 fi
 
 cd "$TOOL_DIRECTORY" || exit 1
@@ -83,8 +83,8 @@ DATADOG_CLI_PATH=/usr/bin/datadog-ci
 
 # Check that datadog-ci was installed
 if [ ! -x $DATADOG_CLI_PATH ]; then
-    echo "The datadog-ci was not installed correctly, not found in $DATADOG_CLI_PATH."
-    exit 1
+	echo "The datadog-ci was not installed correctly, not found in $DATADOG_CLI_PATH."
+	exit 1
 fi
 
 echo "Done: datadog-ci available $DATADOG_CLI_PATH"
@@ -98,8 +98,8 @@ OUTPUT_DIRECTORY=$(mktemp -d)
 
 # Check that datadog-ci was installed
 if [ ! -d "$OUTPUT_DIRECTORY" ]; then
-    echo "Output directory ${OUTPUT_DIRECTORY} does not exist"
-    exit 1
+	echo "Output directory ${OUTPUT_DIRECTORY} does not exist"
+	exit 1
 fi
 
 OUTPUT_FILE="$OUTPUT_DIRECTORY/output.sarif"
@@ -116,17 +116,17 @@ git config --global --add safe.directory ${GITHUB_WORKSPACE} || exit 1
 
 # Only upload git metadata if diff aware is enabled.
 if [ "$DIFF_AWARE" = "true" ]; then
-    echo "Disabling extensions.worktreeConfig"
-    git config --unset extensions.worktreeConfig
-    echo "Done"
+	echo "Disabling extensions.worktreeConfig"
+	git config --unset extensions.worktreeConfig
+	echo "Done"
 
-    echo "Upload git metadata"
-    ${DATADOG_CLI_PATH} git-metadata upload
-    echo "Done"
+	echo "Upload git metadata"
+	${DATADOG_CLI_PATH} git-metadata upload
+	echo "Done"
 fi
 
 echo "Starting Static Analysis"
-$CLI_LOCATION -i "$GITHUB_WORKSPACE" -g -o "$OUTPUT_FILE" -f sarif --cpus "$CPU_COUNT" "$ENABLE_PERFORMANCE_STATISTICS" --debug $DEBUG_ARGUMENT_VALUE $SUBDIRECTORY_OPTION $DIFF_AWARE_VALUE|| exit 1
+$CLI_LOCATION -i "$GITHUB_WORKSPACE" -g -o "$OUTPUT_FILE" -f sarif --cpus "$CPU_COUNT" "$ENABLE_PERFORMANCE_STATISTICS" --debug $DEBUG_ARGUMENT_VALUE $SUBDIRECTORY_OPTION $DIFF_AWARE_VALUE || exit 1
 echo "Done"
 
 echo "Uploading Static Analysis Results to Datadog"
@@ -137,5 +137,5 @@ echo "Done"
 # SCA/SBOM
 ########################################################
 if [ "${SCA_ENABLED}" = "true" ] || [ "${SCA_ENABLED}" = "yes" ]; then
-  echo "SCA no longer supported in this GitHub action, use https://github.com/DataDog/datadog-sca-github-action"
+	echo "SCA no longer supported in this GitHub action, use https://github.com/DataDog/datadog-sca-github-action"
 fi
